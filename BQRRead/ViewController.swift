17c7d96f501f7c63472f7898830cab68929dc70d
RES <info@neviplay.com>
2020-11-01 15:03:41 +0300

    Initial Commit

---
+++ BQRRead/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BQRRead
+//
+//  Created by Resul Emül on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

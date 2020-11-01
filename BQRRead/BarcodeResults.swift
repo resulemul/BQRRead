843ebd60c41523170a64e77ec1e81216fbed6ca2
RES <info@neviplay.com>
2020-11-01 15:27:42 +0300

    first commit

---
+++ BQRRead/BarcodeResults.swift
@@ -0,0 +1,20 @@
+//
+//  BarcodeResults.swift
+//  BQRRead
+//
+//  Created by Resul Emül on 11/1/20.
+//
+
+import UIKit
+
+class BarcodeResults: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
